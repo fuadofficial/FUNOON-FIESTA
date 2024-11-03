785272476bd6f3231191374bc2121ac9017d83cd
unknown <ahmadfuadoorakam@gmail.com>
2024-11-03 17:39:27 +0530

    completed navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,27 +19,27 @@ const Navbar = () => {
     window.addEventListener('resize', handleResize);
 
     return (
-        <header className="bg-gray-800 shadow-lg fixed w-full z-20 h-16 top-0 left-0">
+        <header className="bg-transparent mt-10 fixed w-full z-20 h-16 top-0 left-0">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     {/* Logo */}
-                    <div className="flex-shrink-0 text-white font-bold text-2xl">
-                        <Link to="/">Funoon fiesta</Link>
+                    <div className="flex-shrink-0 text-black text-3xl font-extrabold">
+                        <Link to="/">LOGO</Link>
                     </div>
 
                     {/* Desktop Menu */}
-                    <nav className="hidden md:flex space-x-10">
-                        <Link to="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                            Home
+                    <nav className="hidden md:flex ">
+                        <Link to="/" className="text-gray-900 hover:text-green-900 px-3 py-2 rounded-md text-lg font-medium">
+                            HOME
                         </Link>
-                        <Link to="/about" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                            About
+                        <Link to="/about" className="text-gray-900 hover:text-green-900 px-3 py-2 rounded-md text-lg font-medium">
+                            ABOUT
                         </Link>
-                        <Link to="/services" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                            Services
+                        <Link to="/services" className="text-gray-900 hover:text-green-900 px-3 py-2 rounded-md text-lg font-medium">
+                            RESULT
                         </Link>
-                        <Link to="/contact" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                            Contact
+                        <Link to="/contact" className="text-gray-900 hover:text-green-900 px-3 py-2 rounded-md text-lg font-medium">
+                            CONTACT
                         </Link>
                     </nav>
 
@@ -49,7 +49,7 @@ const Navbar = () => {
                             onClick={toggleMenu}
                             className="text-gray-300 hover:text-white focus:outline-none"
                         >
-                            {isOpen ? <XIcon className="h-6 w-6 " /> : <MenuIcon className="h-6 w-6" />}
+                            {isOpen ? <XIcon className="h-6 w-6 font-black" /> : <MenuIcon className="h-6 w-6 font-black" />}
                         </button>
                     </div>
                 </div>
@@ -59,32 +59,32 @@ const Navbar = () => {
             {isOpen && (
                 <div className="relative">
                     {isOpen && (
-                        <div className="absolute -top-4 right-6  rounded-md bg-white shadow-xl w-40 text-center">
+                        <div className="absolute -top-4 right-6  rounded-md bg-white font-medium shadow-xl w-40 text-center">
                             <nav className="px-2 pt-2 pb-4 space-y-1">
                                 <Link
                                     to="/"
-                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-600"
+                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-blue-900"
                                     onClick={toggleMenu}
                                 >
                                     Home
                                 </Link>
                                 <Link
                                     to="/about"
-                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-600"
+                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-blue-900"
                                     onClick={toggleMenu}
                                 >
                                     About
                                 </Link>
                                 <Link
                                     to="/services"
-                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-600"
+                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-blue-900"
                                     onClick={toggleMenu}
                                 >
                                     Services
                                 </Link>
                                 <Link
                                     to="/contact"
-                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-gray-600"
+                                    className="block px-3 py-2 rounded-md text-base font-medium hover:text-white hover:bg-blue-900"
                                     onClick={toggleMenu}
                                 >
                                     Contact
